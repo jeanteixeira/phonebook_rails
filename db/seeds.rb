524829a3dff9eb733b4726c88bcfb5820f1e6338
Jean B <jean.barros.teixeira@gmail.com>
2020-10-25 15:51:18 -0300

    working with the seed file and the Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Gerando os tipos de contatos (Kinds)"
+  Kind.create!([{description: "Amigo"},
+              {description: "Comercial"},
+              {description: "Família"}])
+puts "Gerando os tipos de contatos (Kinds)... [OK]"
+
+
+puts "Gerando os contatos (Contacts)"
+10.times do |i|
+  Contact.create!(
+    name: Faker::Name.name,                   
+    email: Faker::Internet.email,                   
+    kind: Kind.all.sample,                   
+    rmk: Faker::Lorem.paragraph([1,2,3].sample)
+    )
+end
+puts "Gerando os contatos (Contacts)... [OK]"
